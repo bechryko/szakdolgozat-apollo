dfe7d4ef379d72783fccf4cf95fdbe00d09e5ae0
Kozma Kristof <bechryko@gmail.com>
2024-02-27 19:02:24 +0100

    Set up workspace

--- apollo/src/app/app.component.ts
+++ apollo/src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'apo-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'apo-root',
+   standalone: true,
+   imports: [
+      CommonModule,
+      RouterOutlet
+   ],
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss'],
+   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'apollo';
 }
